2d6f1d854432e0b210cd76823a4710750e031d02
byungjin <byungjin.dev@gmail.com>
2023-02-19 14:40:59 +0900

    feat: Change Browser into HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import RootBoundary from "@/pages/RootBoundary";
 import queryClient, { QueryClientProvider } from "@/query";
-import { BrowserRouter as RootRouter } from "react-router-dom";
+import { HashRouter as RootRouter } from "react-router-dom";
 import { BASE_NAME } from "./constants";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RootRouter basename={BASE_NAME}>
+    <RootRouter /* basename={BASE_NAME} */>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
           <RootBoundary>
