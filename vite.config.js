1a17c15c41692fc1997c912f77fece5996b781b6
byungjin <byungjin.dev@gmail.com>
2023-02-21 13:53:08 +0900

    chore: Set Vite Server Port

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { BASE_NAME } from "./src/constants";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { BASE_NAME } from './src/constants';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: BASE_NAME, // 반드시 배포할 때는 수정해야함.
   plugins: [
     react({
-      fastRefresh: process.env.NODE_ENV !== "test",
+      fastRefresh: process.env.NODE_ENV !== 'test',
     }),
   ],
+  server: {
+    port: 3001,
+  },
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
       },
     ],
   },
   build: {
     commonjsOptions: {
-      exclude: ["./src/__mocks__", "./src/__test__"],
+      exclude: ['./src/__mocks__', './src/__test__'],
     },
   },
 });
