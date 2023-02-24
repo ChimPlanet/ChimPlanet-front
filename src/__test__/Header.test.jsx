8fd835769a2ea396e8b3c9fe2664539342469227
byungjin <byungjin.dev@gmail.com>
2023-02-25 01:25:37 +0900

    fix: Fix Header useHref Error(#10)

--- src/__test__/Header.test.jsx
+++ src/__test__/Header.test.jsx
@@ -1,15 +1,16 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Header from '@/components/Header';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Header Component', () => {
   test('정상적으로 Logo 문자열이 출력된다.', () => {
-    render(<Header />);
+    render(<Header />, { wrapper: MemoryRouter });
     let el = screen.getByText(/침플래닛/);
     expect(el).toBeInTheDocument();
   });
 
   test('태그 검색을 클릭하면, 태그 검색창이 뜬다.', () => {
-    render(<Header />);
+    render(<Header />, { wrapper: MemoryRouter });
     const el = screen.getByPlaceholderText(/#태그 검색/);
     fireEvent.click(el);
 
