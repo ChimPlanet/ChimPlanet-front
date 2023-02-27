15a11b836799e56a24ced9684230ea97b7c9fb94
byungjin <byungjin.dev@gmail.com>
2023-02-27 13:19:07 +0900

    fix: Fix Header Background Sheet Click Capture(#10)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -24,7 +24,7 @@ const BackgroundSheet = styled.div`
   width: 100%;
   height: 100%;
   position: fixed;
-  z-index: 0;
+  z-index: 1;
   top: 0;
   left: 0;
   background-color: rgba(0, 0, 0, 0.107);
