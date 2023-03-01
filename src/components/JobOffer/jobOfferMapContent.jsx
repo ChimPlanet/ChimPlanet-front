11580f3b8ae0fbcd60583ec5bd4f0611ddf47323
byungjin <byungjin.dev@gmail.com>
2023-03-01 19:07:01 +0900

    feature: Add Bookmark Page(#28)

--- src/components/JobOffer/jobOfferMapContent.jsx
+++ src/components/JobOffer/jobOfferMapContent.jsx
@@ -1,8 +1,16 @@
 import JobOffer from '@/components/JobOffer';
+import { BookmarkContext } from '@/utils/Context/bookmarkContext';
+import { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import useBookmark from '@/hooks/useBookmark';
+
 /**
- * @param {{jobs: import("@/utils/job").JobOfferVO[]}}
+ * @param {{jobs: import("@/utils/job").JobOfferVO[], toggleBookmark(id: number):void}}
  */
+
 export default function JobOfferMapContent({ jobs }) {
+  const { toggle } = useBookmark();
+  const bookmarkSet = BookmarkContext.getInstance().getBookmarkSet();
   return (
     <>
       {jobs.map((offer) => (
@@ -14,10 +22,11 @@ export default function JobOfferMapContent({ jobs }) {
           writeAt={offer.regDate}
           thumbnailUrl="https://cafeptthumb-phinf.pstatic.net/MjAyMzAyMDhfMTI1/MDAxNjc1ODY1OTk1MjUx.APGGjNqh9LS6w7tSLOcMAxn6_gAlP6INceA8x2q50Pog.xW5jRHAnSA2q_i7KN7to7wWTZKQPW3s3nk823D8u614g.JPEG/0001.jpg?type=w1600"
           viewCount={offer.viewCount}
-          isBookmarked={false}
+          isBookmarked={bookmarkSet.has(offer.boardId)}
           isClosed={offer.isClosed}
           isCreate={offer.isCreate}
           isRegular={offer.isRegular}
+          onBookmarkClick={() => toggle(offer.boardId)}
           style={{
             width: 250,
           }}
@@ -26,3 +35,7 @@ export default function JobOfferMapContent({ jobs }) {
     </>
   );
 }
+
+JobOfferMapContent.propTypes = {
+  jobs: PropTypes.array,
+};
--- src/query/job.js
+++ src/query/job.js
@@ -2,11 +2,23 @@ import { useQuery } from 'react-query';
 import { JobOfferQueryKey } from '@/constants/query';
 import fetchJobOffer from '@/api/job/fetchJobOffer';
 import JobUtils from '@/utils/job';
+import { useCallback } from 'react';
 
 const transformResponse = (data) => data.map(JobUtils.__transform);
 
 export const usePopularJobOffer = () => {
   return useQuery([JobOfferQueryKey], fetchJobOffer, {
     select: transformResponse,
+  });
+};
+
+export const useJobOfferByArrayId = (ids) => {
+  return useQuery([JobOfferQueryKey], fetchJobOffer, {
+    select: useCallback(
+      (data) => {
+        return transformResponse(data).filter((el) => ids.includes(el.boardId));
+      },
+      [ids],
+    ),
   });
 };
