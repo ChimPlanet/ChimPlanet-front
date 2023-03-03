51748fa9e9515dee27dbabcdd120946256dcb584
byungjin <byungjin.dev@gmail.com>
2023-03-03 16:46:00 +0900

    refactor: 카테고리 내 TagTrie 이용을 위한 TagList 호출 위치 변경(#25)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Theme from '@/components/Theme';
 import '@/styles/App.scss';
 import styled from 'styled-components';
 import Header from './components/Header';
+import { useEffect } from 'react';
+import fetchTagList from '@/api/tag/fetchTagList';
+import TagTrie from '@/utils/tagTrie';
 
 const Main = styled.main`
   color: ${({ theme }) => theme.colors.main};
@@ -10,6 +13,11 @@ const Main = styled.main`
 `;
 
 function App() {
+  // ! 단 한번 필요한 데이터를 초기화
+  useEffect(() => {
+    fetchTagList().then((tags) => TagTrie.getInstance(tags));
+  }, []);
+
   return (
     <Theme>
       <Main id="App">
