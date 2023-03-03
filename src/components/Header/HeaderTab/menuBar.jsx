c0f45a0e9a9aa810e3666f929a0eec019ac32a69
byungjin <byungjin.dev@gmail.com>
2023-03-03 18:04:42 +0900

    feature: Add MenuItem Underline when hover, route(#25)

--- src/components/Header/HeaderTab/menuBar.jsx
+++ src/components/Header/HeaderTab/menuBar.jsx
@@ -1,31 +1,56 @@
 import styled from 'styled-components';
 import MenuIcon from '@/components/icons/MenuIcon';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { EVENT_PATH, OFFICIAL_PATH } from '@/constants/route';
 
 const Container = styled.div`
-  margin-top: 25px;
+  position: relative;
+  color: #444444;
+  font-size: 14px;
+  font-weight: 700;
+  margin-top: 30px;
 `;
 
-const MenuItem = styled.span`
+const MenuItem = styled.div`
+  cursor: pointer;
   display: inline-block;
-  font-weight: 700;
-  font-size: 14px;
-  vertical-align: middle;
-  width: 100px;
-  margin-left: 10px;
+  text-align: center;
+  height: 35px;
+  user-select: none;
+  padding: 0px 23px;
+
+  border-bottom: ${({ theme, active }) =>
+    active ? `2px solid ${theme.colors.logo}` : 'none'};
+
+  &:hover {
+    border-bottom: ${({ theme }) => `2px solid ${theme.colors.border}`};
+  }
+`;
+
+const CategoryItem = styled(MenuItem)`
+  padding-left: 0;
+`;
+
+const MenuLink = styled(Link)`
+  display: block;
+  width: 100%;
+  height: 100%;
 `;
 
 export default function MenuBar() {
+  const { pathname } = useLocation();
+
   return (
     <Container>
-      <MenuIcon />
-      <MenuItem>카테고리</MenuItem>
-      <MenuItem>
-        <Link to={EVENT_PATH}>이벤트</Link>
+      <CategoryItem>
+        <MenuIcon />
+        &nbsp;&nbsp; 카테고리
+      </CategoryItem>
+      <MenuItem active={pathname === EVENT_PATH}>
+        <MenuLink to={EVENT_PATH}>이벤트</MenuLink>
       </MenuItem>
-      <MenuItem>
-        <Link to={OFFICIAL_PATH}>공식</Link>
+      <MenuItem active={pathname == OFFICIAL_PATH}>
+        <MenuLink to={OFFICIAL_PATH}>공식</MenuLink>
       </MenuItem>
     </Container>
   );
