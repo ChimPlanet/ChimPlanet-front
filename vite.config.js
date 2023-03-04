f2ce3aa4170939dadd070635a44fb536119b4d60
ByungJin Lee <Byungjin.dev@gmail.com>
2023-03-05 00:23:40 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { BASE_NAME } from './src/constants';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // loadEnv(mode, process.cwd());
   return {
-    base: BASE_NAME, // 반드시 배포할 때는 수정해야함.
     plugins: [
       react({
         fastRefresh: process.env.NODE_ENV !== 'test',
