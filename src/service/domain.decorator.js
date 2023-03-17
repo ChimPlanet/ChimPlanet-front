b0cb6a0b153f6fafdf4d51db1267d0e3cf599c9f
byungjin <byungjin.dev@gmail.com>
2023-03-18 02:49:02 +0900

    feature: API Banner Endpoint HttpClient Decorator 생성(#56)

--- src/service/domain.decorator.js
+++ src/service/domain.decorator.js
@@ -26,3 +26,18 @@ export function field(responseFieldName, preprocess) {
     };
   };
 }
+
+export function select(type) {
+  return function (target, name, { value: fn, configurable, enumerable }) {
+    return {
+      configurable,
+      enumerable,
+      get() {
+        return async (...args) => {
+          const result = await fn.call(this, args);
+          return type(result);
+        };
+      },
+    };
+  };
+}
