10d0bf2bc5d6cf52a829d5b58c84b1a5a56e921a
spde3289 <spde3289@naver.com>
2023-03-19 20:57:09 +0900

    features: API에 맞게 수정(#14)

--- src/components/JobInfiniteScroll/index.jsx
+++ src/components/JobInfiniteScroll/index.jsx
@@ -1,10 +1,7 @@
 import { useMemo } from 'react';
 import styled from 'styled-components';
 import { useSizeType } from '@/context/sizeTypeContext';
-import { JobOffer } from '@/common/components/JobOffer';
-import { BookmarkContext } from '@/utils/Context/bookmarkContext';
-import useBookmark from '@/hooks/useBookmark';
-import { useArticleContext } from '@/context/articleContext';
+import { JobOfferMapContent } from '@/common/components/JobOffer';
 
 const JobOfferContainer = styled.div`
   display: grid;
@@ -15,35 +12,12 @@ const JobOfferContainer = styled.div`
 `;
 
 export default function JobInfiniteScroll({ List }) {
-  const [, { open }] = useArticleContext();
   const sizeType = useSizeType();
   const pageCount = useMemo(() => (sizeType === 'desktop' ? 4 : 3), [sizeType]);
-  const { toggle } = useBookmark();
-  const bookmarkSet = BookmarkContext.getInstance().getBookmarkSet();
-
   return (
     <>
       <JobOfferContainer column={pageCount}>
-        {List.map((item) => (
-          <JobOffer
-            onClick={() => open(+item.articleId)}
-            key={item.articleId}
-            id={+item.articleId}
-            title={item.boardTitle}
-            isRegular={false}
-            writer={item.writer}
-            writeAt={item.regDate}
-            isThumbnail={false}
-            thumbnailURL={item.thumbnailURL}
-            viewCount={+item.readCount.replace(',', '')}
-            isClosed={item.isEnd === 'ING' ? false : true}
-            isBookmarked={bookmarkSet.has(item.articleId)}
-            onBookmarkClick={() => toggle(item.articleId)}
-            style={{
-              width: 250,
-            }}
-          />
-        ))}
+        <JobOfferMapContent jobs={List} />
       </JobOfferContainer>
     </>
   );
