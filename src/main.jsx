9283200e859847c6c54a9409e055517b788093eb
byungjin <byungjin.dev@gmail.com>
2023-04-06 02:50:02 +0900

    chore: Disable Strict Mode for React Beautiful DND

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,13 @@ import queryClient, { QueryClientProvider } from '@/query';
 import { BrowserRouter as RootRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <RootRouter /* basename={BASE_NAME} */>
-          <RootBoundary>
-            <App />
-          </RootBoundary>
-        </RootRouter>
-      </RecoilRoot>
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <RootRouter /* basename={BASE_NAME} */>
+        <RootBoundary>
+          <App />
+        </RootBoundary>
+      </RootRouter>
+    </RecoilRoot>
+  </QueryClientProvider>,
 );
