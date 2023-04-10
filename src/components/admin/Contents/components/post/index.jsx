a2291713165b153c4f08b9db3746e7ba22b38a71
spde3289 <spde3289@naver.com>
2023-04-10 09:35:40 +0900

    fix: 버그 수정(#64)

--- src/components/admin/Contents/components/post/index.jsx
+++ src/components/admin/Contents/components/post/index.jsx
@@ -1,6 +1,4 @@
 import { useState, useMemo } from 'react'
-import { useSetRecoilState, useRecoilValue } from "recoil";
-import { ListSort } from "@/atoms/PostList";
 import ContnetsTab from "./ContentsTab";
 import ContentsOfferSection from "./ContentsOfferSection";
 import { useRecentOffers } from '@/query/offer';
@@ -11,19 +9,16 @@ export default function ContentsPosts(){
     const [newList, setNewList] = useState([]);
     const [select, setSelect] = useState(false);
     const [selectValue, setSelectValue] = useState('최신순');
-    const listSort = useRecoilValue(ListSort);
-    const sort = useSetRecoilState(ListSort);
-
+   
     const { data } = useRecentOffers();
 
     const onSelect = () => {
-        setSelect(!select);
+      setSelect(!select);
     };
-
-    const setValue = (value) => {
-        setSelectValue(value);
-        sort(value);
-        onSelect();
+  
+    const setValue = (e) => {
+      setSelectValue(e);
+      setSelect(!select);
     };
 
     useMemo(()=>{
@@ -32,18 +27,18 @@ export default function ContentsPosts(){
 
     useMemo(() => {
         const newPostList = [...postList];
-        if (listSort === '조회순') {
+        if (selectValue === '조회순') {
           setNewList(
             newPostList.sort((a, b) => b.data.readCount - a.data.readCount)
           );
-        } else if (listSort === '추천순') {
+        } else if (selectValue === '추천순') {
           setNewList(
             newPostList.sort((a, b) => b.data.likeCount - a.data.likeCount)
           );
         } else {
           setNewList(postList);
         }
-    }, [listSort, postList]);
+    }, [selectValue, postList]);
 
     return(
         <>
