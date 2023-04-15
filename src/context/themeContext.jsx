741ab3fe72f872ce045a4a030363e13846b8cf6a
byungjin <byungjin.dev@gmail.com>
2023-04-16 05:22:42 +0900

    feature: Add Detection User Theme Setting(#8)

--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider as __ThemeProvider } from 'styled-components';
 
@@ -11,5 +12,38 @@ ThemeProvider.propTypes = {
  * @param {{children: JSX.Element}}
  */
 export function ThemeProvider({ children }) {
-  return <__ThemeProvider theme={lightTheme}>{children}</__ThemeProvider>;
+  const [theme] = useTheme();
+
+  return (
+    <__ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      {children}
+    </__ThemeProvider>
+  );
+}
+
+export function useTheme() {
+  // browser theme information
+  const isBrowserDarkMode =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+  let initTheme = isBrowserDarkMode ? 'dark' : 'light';
+
+  // custom theme mode
+  const userTheme = localStorage.getItem('theme');
+
+  // priority
+  if (userTheme) {
+    initTheme = userTheme;
+  }
+
+  const [theme, setTheme] = useState(initTheme);
+
+  const setMode = (mode) => {
+    window.localStorage.setItem('theme', mode);
+    setTheme(mode);
+  };
+
+  const toggleTheme = () => setMode(theme === 'light' ? 'dark' : 'light');
+
+  return [theme, toggleTheme];
 }
