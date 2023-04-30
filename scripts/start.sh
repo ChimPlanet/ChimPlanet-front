2fdee45465c2a9ef20666c24a7d19ddd5ee8bc14
ByungJin Lee <Byungjin.dev@gmail.com>
2023-04-30 22:53:13 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,8 @@
 
 APP_NAME="fe-user-staged"
 
+projectPath=$(pwd)
+
 sudo pm2 stop ${APP_NAME}
 
-sudo pm2 serve ./ --spa --name ${APP_NAME} --port 1008
+sudo pm2 serve ${projectPath} --spa --name ${APP_NAME} --port 1008
